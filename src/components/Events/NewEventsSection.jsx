fe7be685739fab6c21d6e3bf65d2c717312a60a0
Bubsworth <bubbard96@hotmail.com>
2024-03-25 16:08:30 +0000

    Dynamic Query Functions & Query Keys

--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -1,14 +1,16 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery } from "@tanstack/react-query";
 
-import LoadingIndicator from '../UI/LoadingIndicator.jsx';
-import ErrorBlock from '../UI/ErrorBlock.jsx';
-import EventItem from './EventItem.jsx';
-import { fetchEvents } from '../../util/http.js';
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import EventItem from "./EventItem.jsx";
+import { fetchEvents } from "../../util/http.js";
 
 export default function NewEventsSection() {
   const { data, isPending, isError, error } = useQuery({
-    queryKey: ['events'], 
-    queryFn: fetchEvents
+    queryKey: ["events"],
+    queryFn: fetchEvents,
+    staleTime: 5000,
+    // gcTime: 30000
   });
 
   let content;
@@ -19,7 +21,7 @@ export default function NewEventsSection() {
 
   if (isError) {
     content = (
-      <ErrorBlock 
+      <ErrorBlock
         title="An error occurred"
         message={error.info?.message || "Failed to fetch events."}
       />
